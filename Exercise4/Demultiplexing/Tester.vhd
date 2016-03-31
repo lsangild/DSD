eca8da4f633664c810d80f36bceb1b01385a5b00
Munke <aalsing@gmail.com>
2016-03-31 08:58:58 +0200

    added hex3 just to clear it in the tester

--- Exercise4/Demultiplexing/Tester.vhd
+++ Exercise4/Demultiplexing/Tester.vhd
@@ -5,9 +5,9 @@ use ieee.numeric_std.all;
 
 -----Entity-----
 entity tester is
-	port(	KEY					: in std_logic_vector(1 downto 0);
+	port(	KEY						: in std_logic_vector(1 downto 0);
 			SW						: in std_logic_vector(11 downto 0);
-			HEX0, HEX1, HEX2	: out std_logic_vector(6 downto 0)
+			HEX0, HEX1, HEX2, HEX3	: out std_logic_vector(6 downto 0)
 	);
 end tester;
 
@@ -15,6 +15,7 @@ end tester;
 architecture TEST of tester is
 signal data_val	:	std_logic_vector(20 downto 0) := "111111111111111111111";
 begin
+HEX3 <= "1111111";
 MX		: entity work.Multiplexer		port map (data => data_val, bin_in => SW, ab => KEY);
 DMX	: entity work.Demultiplexer	port map (data_in => data_val, disp0 => HEX0, disp1 => HEX1, disp2 => HEX2);
 end TEST;
