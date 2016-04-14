8d14cf5d5c1e677290e4b71bf879fd1ba3e54338
Lasse Sangild <lsangild+github@gmail.com>
2016-04-14 13:13:49 +0200

    Kan skifte mellem players og players kan ikke se hinandens koder. Kan dog kun gemme en kode ad gangen

--- Exercise5/GuessGame/TesterGuessGame2P.vhd
+++ Exercise5/GuessGame/TesterGuessGame2P.vhd
@@ -4,10 +4,9 @@ use ieee.std_logic_1164.all;
 
 -----Entity declaration -----
 entity TesterGuessGame2P is
-	port(	SW									: in std_logic_vector(7 downto 0);
-			SW17								: in std_logic;
+	port(	SW												: in std_logic_vector(8 downto 0);
 			HEX0, HEX1, HEX2, HEX3, HEX4, HEX5	: out std_logic_vector(6 downto 0);
-			KEY									: in std_logic_vector(2 downto 0)
+			KEY											: in std_logic_vector(2 downto 0)
 	);	
 end TesterGuessGame2P;
 
@@ -17,31 +16,34 @@ architecture guessing of TesterGuessGame2P is
 constant turnOff : std_logic_vector(6 downto 0) := "1111111";
 constant P		 : std_logic_vector(6 downto 0) := "0001100";
 signal p1Hex1, p1Hex10, p2Hex1, p2Hex10 : std_logic_vector(6 downto 0);
+signal p1Code, p2Code : std_logic_vector(7 downto 0);
 begin
 HEX2 <= turnOff;
 HEX3 <= turnOff;
 HEX5 <= P;
 -- architecture body...
-P1:	entity work.GuessGame port map (	inputs => SW, set => KEY(0), 
+P1: entity work.GuessGame port map (	inputs => p1Code, set => KEY(0), 
 										show => KEY(1), try => KEY(2), 
 										hex1 => p1Hex1, hex10 => p1Hex10);
 
-P2: entity work.GuessGame port map (	inputs => SW, set => KEY(0), 
+P2: entity work.GuessGame port map (	inputs => p2Code, set => KEY(0), 
 										show => KEY(1), try => KEY(2), 
 										hex1 => p2Hex1, hex10 => p2Hex10);
--- ide, sæt
-process(SW17)
+
+process(SW(8))
 begin
-	if (SW17 = '0') then
+	if (SW(8) = '0') then
 	  -- Player 1...
 		HEX4 <= "1111001";
 		HEX0 <= p1Hex1;
 		HEX1 <= p1Hex10;
-	  elsif (SW17 = '1') then
-	  -- player 2
+		p1Code <= SW(7 downto 0);
+	  elsif (SW(8) = '1') then
+	  -- Player 2
 		HEX4 <= "0100100";
 		HEX0 <= p2Hex1;
 		HEX1 <= p2Hex10;
+		p2Code <= SW(7 downto 0);
 	  else
 	  --go die --
 		HEX4 <= turnOff;
