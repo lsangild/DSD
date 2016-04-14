ef93e44d426627a1579957d6a66c167a88bb0726
Munke <aalsing@gmail.com>
2016-04-14 09:31:03 +0200

    Part one is now done, the skeleton of part two has begun

--- Exercise5/SevenSegCase/tester.vhd
+++ Exercise5/SevenSegCase/tester.vhd
@@ -5,13 +5,17 @@ use ieee.numeric_std.all;
 
 -----Entity-----
 entity tester is
-	port(	SW	: in std_logic_vector(3 downto 0);
-			HEX1	: out std_logic_vector(6 downto 0)
+	port(	SW						: in std_logic_vector(3 downto 0);
+			HEX0, HEX1, HEX2, HEX3	: out std_logic_vector(6 downto 0)
 	);
 end tester;
 
 -----Architecture-----
 architecture TEST of tester is
+constant turnOff : std_logic_vector(6 downto 0) := "1111111";
 begin
-B2S		: entity work.Decode port map (bin => SW, seg => HEX1);
+HEX1 <= turnOff;
+HEX2 <= turnOff;
+HEX3 <= turnOff;
+B2S		: entity work.SevenSegCase port map (bin => SW, seg => HEX0);
 end TEST;
