336f8e5d9a38074d6e40b5a58a4a9eb69ad9b916
Munke <aalsing@gmail.com>
2016-04-28 09:39:55 +0200

    Added new version of bin2seg file, that is reversed

---
+++ Exercise7/Mee-Moo.vhd
@@ -0,0 +1,11 @@
+----- Libraries -----
+library ieee;
+use ieee.std_logic_1164.all;
+
+
+entity Mee-Moo is
+	port(	
+	
+	);
+
+end Mee-Moo;
---
+++ Exercise7/Vhdl1.vhd
@@ -0,0 +1,5 @@
+----- Libraries -----
+library ieee;
+use ieee.std_logic_1164.all;
+
+
