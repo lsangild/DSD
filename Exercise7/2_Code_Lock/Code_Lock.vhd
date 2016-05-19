ae8348bd3738688d27205f081da173fd47314d98
Munke <aalsing@gmail.com>
2016-05-19 11:54:55 +0200

    Laala

--- Exercise7/2_Code_Lock/Code_Lock.vhd
+++ Exercise7/2_Code_Lock/Code_Lock.vhd
@@ -1,21 +1,24 @@
 ----- Libraries -----
 library ieee;
 use ieee.std_logic_1164.all;
-
+use ieee.numeric_std.all;
 
 entity Code_Lock is
-	port(	clk	:	in std_logic;
+	port(	clk		:	in std_logic;
 			reset	:	in std_logic;
 			code	:	in std_logic_vector(3 downto 0);
 			enter	:	in std_logic;
-			lock	:	out std_logic
+			lock	:	out std_logic;
+			err		:	out std_logic_vector(1 downto 0)
 			);
 end Code_Lock;
 
 architecture simple of Code_Lock is
 type state is (idle, eval1, get2, eval2, unlocked, going_idle, wcode, permlock);
-type stateE is (Err_1, Err_2, Err_3);
+type state2 is (Err_0, Err_1, Err_2, Err_3);
 signal present_state, next_state : state;
+signal code_lock_present_state, code_lock_next_state : state2;
+signal err_cnt : std_logic_vector(1 downto 0) := "00";
 begin
 
 state_reg: process(clk, reset)
@@ -23,7 +26,16 @@ begin
 	if reset = '0' then
 		present_state <= idle;
 	elsif rising_edge(clk) then
-		present_state <= next_state;
+		present_state <= next_state;		
+	end if;
+end process;
+
+lock_reg: process(clk, reset)
+begin
+	if reset = '0' then
+		code_lock_present_state <= Err_0;
+	elsif rising_edge(clk) then
+		code_lock_present_state <= code_lock_next_state;		
 	end if;
 end process;
 
@@ -60,14 +72,51 @@ begin
 				next_state <= idle;
 			end if;
 		when wcode =>
-			if enter = '0' then
+			if err_cnt = "10" then
 				next_state <= permlock;
+			else
+				next_state <= going_idle;
 			end if;
 		when permlock =>
 			null;
 		when others =>
 			next_state <= idle;
 	end case;
+end process;
+
+
+wrongcode: process(present_state,code_lock_present_state, next_state)
+begin
+if present_state = wcode and next_state /= wcode then
+  case code_lock_present_state is
+	when Err_0 =>
+		code_lock_next_state <= Err_1;
+	when Err_1 =>
+		code_lock_next_state <= Err_2;
+	when Err_2 =>
+		code_lock_next_state <= Err_3;
+	when Err_3 =>
+		code_lock_next_state <= Err_0;
+	when others =>
+	  -- Do stuff for everything else;
+	  NULL;
+  end case;
+ end if;
+end process; 
+
+lock_out: process(code_lock_present_state)
+begin
+	case code_lock_present_state is
+		when Err_0 =>
+			err_cnt <= "00";
+		when Err_1 =>
+			err_cnt <= "01";
+		when Err_2 =>
+			err_cnt <= "10";
+		when Err_3 =>
+			err_cnt <= "11";
+	end case;
+	err <= err_cnt;
 end process;
 
 outputs: process(present_state)
@@ -79,5 +128,4 @@ begin
 			lock <= '1';
 		end case;
 end process;
-
 end simple;
