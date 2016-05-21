32ab06b24924de138503b68dd52ba3c81e31e6b4
Lasse Sangild <lsangild+github@gmail.com>
2016-05-21 12:36:05 +0200

    Ændret koden for den anden state-machine til at ligne den i listing 10.9.1, side 424

--- Exercise7/2_Code_Lock/Code_Lock.vhd
+++ Exercise7/2_Code_Lock/Code_Lock.vhd
@@ -4,21 +4,25 @@ use ieee.std_logic_1164.all;
 use ieee.numeric_std.all;
 
 entity Code_Lock is
-	port(	clk		:	in std_logic;
+	port(	clk	:	in std_logic;
 			reset	:	in std_logic;
 			code	:	in std_logic_vector(3 downto 0);
 			enter	:	in std_logic;
 			lock	:	out std_logic;
-			err		:	out std_logic_vector(1 downto 0)
+			err	:	out std_logic_vector(1 downto 0)
 			);
 end Code_Lock;
 
 architecture simple of Code_Lock is
 type state is (idle, eval1, get2, eval2, unlocked, going_idle, wcode, permlock);
-type state2 is (Err_0, Err_1, Err_2, Err_3);
+subtype state2 is std_logic_vector (1 downto 0);
 signal present_state, next_state : state;
 signal code_lock_present_state, code_lock_next_state : state2;
-signal err_cnt : std_logic_vector(1 downto 0) := "00";
+constant Err_0: state2 := "00";
+constant Err_1: state2 := "01";
+constant Err_2: state2 := "10";
+constant Err_3: state2 := "11";
+
 begin
 
 state_reg: process(clk, reset)
@@ -26,7 +30,7 @@ begin
 	if reset = '0' then
 		present_state <= idle;
 	elsif rising_edge(clk) then
-		present_state <= next_state;		
+		present_state <= next_state;
 	end if;
 end process;
 
@@ -35,7 +39,7 @@ begin
 	if reset = '0' then
 		code_lock_present_state <= Err_0;
 	elsif rising_edge(clk) then
-		code_lock_present_state <= code_lock_next_state;		
+		code_lock_present_state <= code_lock_next_state;
 	end if;
 end process;
 
@@ -72,7 +76,7 @@ begin
 				next_state <= idle;
 			end if;
 		when wcode =>
-			if err_cnt = "10" then
+			if code_lock_next_state = "11" then
 				next_state <= permlock;
 			else
 				next_state <= going_idle;
@@ -88,35 +92,24 @@ end process;
 wrongcode: process(present_state,code_lock_present_state, next_state)
 begin
 if present_state = wcode and next_state /= wcode then
-  case code_lock_present_state is
-	when Err_0 =>
-		code_lock_next_state <= Err_1;
-	when Err_1 =>
-		code_lock_next_state <= Err_2;
-	when Err_2 =>
-		code_lock_next_state <= Err_3;
-	when Err_3 =>
-		code_lock_next_state <= Err_0;
-	when others =>
-	  -- Do stuff for everything else;
-	  NULL;
-  end case;
- end if;
-end process; 
+	case code_lock_present_state is
+		when Err_0 =>
+			code_lock_next_state <= Err_1;
+		when Err_1 =>
+			code_lock_next_state <= Err_2;
+		when Err_2 =>
+			code_lock_next_state <= Err_3;
+		when Err_3 =>
+			code_lock_next_state <= Err_0;
+		when others =>
+			code_lock_next_state <= Err_0;
+	end case;
+end if;
+end process;
 
 lock_out: process(code_lock_present_state)
 begin
-	case code_lock_present_state is
-		when Err_0 =>
-			err_cnt <= "00";
-		when Err_1 =>
-			err_cnt <= "01";
-		when Err_2 =>
-			err_cnt <= "10";
-		when Err_3 =>
-			err_cnt <= "11";
-	end case;
-	err <= err_cnt;
+	err <= code_lock_present_state;
 end process;
 
 outputs: process(present_state)
