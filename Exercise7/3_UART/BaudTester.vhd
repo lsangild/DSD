a60b0c805339da699d3b95572f8c8f2842063716
Lasse Sangild <lsangild+github@gmail.com>
2016-05-23 13:48:19 +0200

    Lavet tester til clock + receiver

--- Exercise7/3_UART/BaudTester.vhd
+++ Exercise7/3_UART/BaudTester.vhd
@@ -1,7 +1,6 @@
 ----- Libraries -----
 library ieee;
 use ieee.std_logic_1164.all;
-use ieee.numeric_std.all;
 
 entity BaudTester is
 	port (	CLOCK_50	: in std_logic;
@@ -11,6 +10,8 @@ entity BaudTester is
 end BaudTester;
 
 architecture test of BaudTester is
+signal clk_115200 : std_logic;
 begin
-gen	: entity work.BaudRateGenerator		port map (clk => CLOCK_50, reset => KEY(0), clk_baud => LEDR(0));
+gen	: entity work.BaudRateGenerator		port map (clk => CLOCK_50, reset => KEY(0), clk_baud => clk_115200);
+LEDR(0) <= clk_115200;
 end test;
